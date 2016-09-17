a8b7593739a7f2ba3bf36ad2508739e91757de7d
Victor Alveflo <victor.alveflo@gmail.com>
2016-09-17 15:48:13 +0200

    added makefile

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require("gulp");
-var coffee = require("gulp-coffee");
-var gutil = require("gutil");
-
-gulp.task("default", () => {
-  gulp.src("./src/**/*.coffee")
-    .pipe(coffee({ "bare": true }).on("error", gutil.log))
-    .pipe(gulp.dest("./lib"));
-});
